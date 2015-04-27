a96a2c65f4137ea42a7f1b0783ab7c070938e36b
sergio <sergio.lucas.lopez@gmail.com>
2015-04-27 16:46:31 +0200

    Initial Commit. BrowseActivity with BaseActivity

---
+++ app/src/main/java/com/example/sergio/quemedejes/ui/BrowseSessionsActivity.java
@@ -0,0 +1,381 @@
+/*
+ * Copyright 2014 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.sergio.quemedejes.ui;
+
+import android.graphics.Paint;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.sergio.quemedejes.Config;
+import com.example.sergio.quemedejes.R;
+import com.example.sergio.quemedejes.ui.widget.DrawShadowFrameLayout;
+import com.example.sergio.quemedejes.util.PrefUtils;
+import com.example.sergio.quemedejes.util.UIUtils;
+
+import java.util.ArrayList;
+
+import static com.example.sergio.quemedejes.util.LogUtils.makeLogTag;
+
+public class BrowseSessionsActivity extends BaseActivity  {
+    private static final String TAG = makeLogTag(BrowseSessionsActivity.class);
+
+    // How is this Activity being used?
+    private static final int MODE_EXPLORE = 0; // as top-level "Explore" screen
+    private static final int MODE_TIME_FIT = 1; // showing sessions that fit in a time interval
+
+    private static final String STATE_FILTER_0 = "STATE_FILTER_0";
+    private static final String STATE_FILTER_1 = "STATE_FILTER_1";
+    private static final String STATE_FILTER_2 = "STATE_FILTER_2";
+
+    public static final String EXTRA_FILTER_TAG = "com.google.android.iosched.extra.FILTER_TAG";
+
+    private int mMode = MODE_EXPLORE;
+
+    private final static String SCREEN_LABEL = "Explore";
+
+    // filter tags that are currently selected
+    private String[] mFilterTags = { "", "", "" };
+
+    // filter tags that we have to restore (as a result of Activity recreation)
+    private String[] mFilterTagsToRestore = { null, null, null };
+
+
+    private DrawShadowFrameLayout mDrawShadowFrameLayout;
+    private View mButterBar;
+
+    // time when the user last clicked "refresh" from the stale data butter bar
+    private long mLastDataStaleUserActionTime = 0L;
+    private int mHeaderColor = 0; // 0 means not customized
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_browse_sessions);
+
+        Toolbar toolbar = getActionBarToolbar();
+
+        overridePendingTransition(0, 0);
+
+        if (savedInstanceState != null) {
+            mFilterTagsToRestore[0] = mFilterTags[0] = savedInstanceState.getString(STATE_FILTER_0);
+            mFilterTagsToRestore[1] = mFilterTags[1] = savedInstanceState.getString(STATE_FILTER_1);
+            mFilterTagsToRestore[2] = mFilterTags[2] = savedInstanceState.getString(STATE_FILTER_2);
+        } else if (getIntent() != null && getIntent().hasExtra(EXTRA_FILTER_TAG)) {
+            mFilterTagsToRestore[0] = getIntent().getStringExtra(EXTRA_FILTER_TAG);
+        }
+
+        if (mMode == MODE_EXPLORE) {
+            // no title (to make more room for navigation and actions)
+            // unless Nav Drawer opens
+            toolbar.setTitle(null);
+        }
+
+        mButterBar = findViewById(R.id.butter_bar);
+        mDrawShadowFrameLayout = (DrawShadowFrameLayout) findViewById(R.id.main_content);
+        registerHideableHeaderView(mButterBar);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        checkShowStaleDataButterBar();
+    }
+
+    @Override
+    public boolean canSwipeRefreshChildScrollUp() {
+        return super.canSwipeRefreshChildScrollUp();
+    }
+
+    private void checkShowStaleDataButterBar() {
+        final boolean showingFilters = findViewById(R.id.filters_box) != null
+                && findViewById(R.id.filters_box).getVisibility() == View.VISIBLE;
+        final long now = UIUtils.getCurrentTime(this);
+        final boolean inSnooze = (now - mLastDataStaleUserActionTime < Config.STALE_DATA_WARNING_SNOOZE);
+        final long staleTime = now - PrefUtils.getLastSyncSucceededTime(this);
+        final long staleThreshold = (now >= Config.CONFERENCE_START_MILLIS && now
+                <= Config.CONFERENCE_END_MILLIS) ? Config.STALE_DATA_THRESHOLD_DURING_CONFERENCE :
+                Config.STALE_DATA_THRESHOLD_NOT_DURING_CONFERENCE;
+        final boolean isStale = (staleTime >= staleThreshold);
+        final boolean bootstrapDone = PrefUtils.isDataBootstrapDone(this);
+        final boolean mustShowBar = bootstrapDone && isStale && !inSnooze && !showingFilters;
+
+        if (!mustShowBar) {
+            mButterBar.setVisibility(View.GONE);
+        } else {
+            UIUtils.setUpButterBar(mButterBar, getString(R.string.data_stale_warning),
+                    getString(R.string.description_refresh), new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mButterBar.setVisibility(View.GONE);
+                            updateFragContentTopClearance();
+                            mLastDataStaleUserActionTime = UIUtils.getCurrentTime(
+                                    BrowseSessionsActivity.this);
+                            requestDataRefresh();
+                        }
+                    }
+            );
+        }
+        updateFragContentTopClearance();
+    }
+
+    @Override
+    protected int getSelfNavDrawerItem() {
+        // we only have a nav drawer if we are in top-level Explore mode.
+        return mMode == MODE_EXPLORE ? NAVDRAWER_ITEM_EXPLORE : NAVDRAWER_ITEM_INVALID;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        registerHideableHeaderView(findViewById(R.id.headerbar));
+    }
+
+    // Updates the Sessions fragment content top clearance to take our chrome into account
+    private void updateFragContentTopClearance() {
+
+        View filtersBox = findViewById(R.id.filters_box);
+
+        final boolean filterBoxVisible = filtersBox != null
+                && filtersBox.getVisibility() == View.VISIBLE;
+        final boolean butterBarVisible = mButterBar != null
+                && mButterBar.getVisibility() == View.VISIBLE;
+
+        int actionBarClearance = UIUtils.calculateActionBarSize(this);
+        int butterBarClearance = butterBarVisible
+                ? getResources().getDimensionPixelSize(R.dimen.butter_bar_height) : 0;
+        int filterBoxClearance = filterBoxVisible
+                ? getResources().getDimensionPixelSize(R.dimen.filterbar_height) : 0;
+        int secondaryClearance = butterBarClearance > filterBoxClearance ? butterBarClearance :
+                filterBoxClearance;
+        int gridPadding = getResources().getDimensionPixelSize(R.dimen.explore_grid_padding);
+
+        setProgressBarTopWhenActionBarShown(actionBarClearance + secondaryClearance);
+        mDrawShadowFrameLayout.setShadowTopOffset(actionBarClearance + secondaryClearance);
+    }
+
+
+    @Override
+    protected void onActionBarAutoShowOrHide(boolean shown) {
+        super.onActionBarAutoShowOrHide(shown);
+        mDrawShadowFrameLayout.setShadowVisible(shown, shown);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.browse_sessions, menu);
+        // remove actions when in time interval mode:
+        if (mMode != MODE_EXPLORE) {
+            menu.removeItem(R.id.menu_search);
+            menu.removeItem(R.id.menu_refresh);
+            menu.removeItem(R.id.menu_wifi);
+            menu.removeItem(R.id.menu_debug);
+            menu.removeItem(R.id.menu_about);
+        } else {
+            configureStandardMenuItems(menu);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_search:
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(STATE_FILTER_0, mFilterTags[0]);
+        outState.putString(STATE_FILTER_1, mFilterTags[1]);
+        outState.putString(STATE_FILTER_2, mFilterTags[2]);
+    }
+
+    private class ExploreSpinnerItem {
+        boolean isHeader;
+        String tag, title;
+        int color;
+        boolean indented;
+
+        ExploreSpinnerItem(boolean isHeader, String tag, String title, boolean indented, int color) {
+            this.isHeader = isHeader;
+            this.tag = tag;
+            this.title = title;
+            this.indented = indented;
+            this.color = color;
+        }
+    }
+
+    /** Adapter that provides views for our top-level Action Bar spinner. */
+    private class ExploreSpinnerAdapter extends BaseAdapter {
+        private int mDotSize;
+        private boolean mTopLevel;
+
+        private ExploreSpinnerAdapter(boolean topLevel) {
+            this.mTopLevel = topLevel;
+        }
+
+        // pairs of (tag, title)
+        private ArrayList<ExploreSpinnerItem> mItems = new ArrayList<>();
+
+        public void clear() {
+            mItems.clear();
+        }
+
+        public void addItem(String tag, String title, boolean indented, int color) {
+            mItems.add(new ExploreSpinnerItem(false, tag, title, indented, color));
+        }
+
+        public void addHeader(String title) {
+            mItems.add(new ExploreSpinnerItem(true, "", title, false, 0));
+        }
+
+        @Override
+        public int getCount() {
+            return mItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        private boolean isHeader(int position) {
+            return position >= 0 && position < mItems.size()
+                    && mItems.get(position).isHeader;
+        }
+
+        @Override
+        public View getDropDownView(int position, View view, ViewGroup parent) {
+            if (view == null || !view.getTag().toString().equals("DROPDOWN")) {
+                view = getLayoutInflater().inflate(R.layout.explore_spinner_item_dropdown,
+                        parent, false);
+                view.setTag("DROPDOWN");
+            }
+
+            TextView headerTextView = (TextView) view.findViewById(R.id.header_text);
+            View dividerView = view.findViewById(R.id.divider_view);
+            TextView normalTextView = (TextView) view.findViewById(android.R.id.text1);
+
+            if (isHeader(position)) {
+                headerTextView.setText(getTitle(position));
+                headerTextView.setVisibility(View.VISIBLE);
+                normalTextView.setVisibility(View.GONE);
+                dividerView.setVisibility(View.VISIBLE);
+            } else {
+                headerTextView.setVisibility(View.GONE);
+                normalTextView.setVisibility(View.VISIBLE);
+                dividerView.setVisibility(View.GONE);
+
+                setUpNormalDropdownView(position, normalTextView);
+            }
+
+            return view;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            if (view == null || !view.getTag().toString().equals("NON_DROPDOWN")) {
+                view = getLayoutInflater().inflate(mTopLevel
+                                ? R.layout.explore_spinner_item_actionbar
+                                : R.layout.explore_spinner_item,
+                        parent, false);
+                view.setTag("NON_DROPDOWN");
+            }
+            TextView textView = (TextView) view.findViewById(android.R.id.text1);
+            textView.setText(getTitle(position));
+            return view;
+        }
+
+        private String getTitle(int position) {
+            return position >= 0 && position < mItems.size() ? mItems.get(position).title : "";
+        }
+
+        private int getColor(int position) {
+            return position >= 0 && position < mItems.size() ? mItems.get(position).color : 0;
+        }
+
+        private String getTag(int position) {
+            return position >= 0 && position < mItems.size() ? mItems.get(position).tag : "";
+        }
+
+        private void setUpNormalDropdownView(int position, TextView textView) {
+            textView.setText(getTitle(position));
+            ShapeDrawable colorDrawable = (ShapeDrawable) textView.getCompoundDrawables()[2];
+            int color = getColor(position);
+            if (color == 0) {
+                if (colorDrawable != null) {
+                    textView.setCompoundDrawables(null, null, null, null);
+                }
+            } else {
+                if (mDotSize == 0) {
+                    mDotSize = getResources().getDimensionPixelSize(
+                            R.dimen.tag_color_dot_size);
+                }
+                if (colorDrawable == null) {
+                    colorDrawable = new ShapeDrawable(new OvalShape());
+                    colorDrawable.setIntrinsicWidth(mDotSize);
+                    colorDrawable.setIntrinsicHeight(mDotSize);
+                    colorDrawable.getPaint().setStyle(Paint.Style.FILL);
+                    textView.setCompoundDrawablesWithIntrinsicBounds(null, null, colorDrawable, null);
+                }
+                colorDrawable.getPaint().setColor(color);
+            }
+
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            return !isHeader(position);
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return 0;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 1;
+        }
+
+        @Override
+        public boolean areAllItemsEnabled() {
+            return false;
+        }
+    }
+}
